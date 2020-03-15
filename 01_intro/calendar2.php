06a6ecb640cec55a4bce92f489d61873d419a9ec
marupahi <mitsuo456.y@gmail.com>
2020-03-15 14:41:01 +0900

    カレンダー更新２

--- 01_intro/calendar2.php
+++ 01_intro/calendar2.php
@@ -1,68 +1,72 @@
+<?php 
+    /*$put days in calendar*/
+    function getCalendar($month) {
+        $calendar = array();
+        $year = 2020;
+        $startDayTimestamp = mktime(0, 0, 0, $month, 1, $year);
+        $endDayTimestamp = mktime(0, 0, 0, $month + 1, 0, $year);
+        $endDay = date("d", $endDayTimestamp);
+        $weekday = date("w", $startDayTimestamp);
+        $endWeekday = date("w", $endDayTimestamp);
+    
+        for ($i = 1; $i <= $endDay; $i++){  
+            /*put blank before 1st in array*/  
+            if ($i == 1){
+                for ($k = 0; $k < $weekday; $k++){
+                    $calendar[] = '';
+                }
+            }
+            /*put 1st to end day in array*/
+            $calendar[] = $i;
+            /*put blank after end day in array */
+            if ($i == $endDay){
+                for ($e = 0; $e < 6 - $endWeekday; $e++){
+                    $calendar[] = '';
+                }
+            }
+        }
+        /*return array*/
+        return $calendar;
+    };
+?>
 
 <!DOCTYPE HTML>
 <html>
     <head>
         <title>Calendar</title>
-        <meta charset = "utf-8">
+        <meta charset="utf-8">
     </head>
     
     <body>
-        <?php for($month=1; $month <= 12; $month ++): ?>
-          <header><?php echo "{$month}月のカレンダー"; ?></header>
+        <?php for ($month=1; $month <= 12; $month++): ?>
+            <h1><?= "{$month}"; ?>月のカレンダー</h1>
 
-          <table border="1">
-            <tr>
-            <th>SUN</th>
-            <th>MON</th>
-            <th>TUE</th>
-            <th>WED</th>
-            <th>THU</th>
-            <th>FRI</th>
-            <th>SAT</th>
-            </tr>
-            <tr>
-
-            <?php //nnnn($calendarMonth); 
-                $calendar = array();
-                $year = 2020;
-                $startDayTimestamp = mktime(0,0,0,$month,1,$year);
-                $endDayTimestamp = mktime(0,0,0,$month+1,0,$year);
-                $endDay = date("d", $endDayTimestamp);
-                $Weekday = date("w", $startDayTimestamp);                
-                $endWeekday = date("w", $endDayTimestamp);
-                for($i = 1; $i <= $endDay; $i++){    
-                    if($i == 1){
-                        for($k = 0; $k < $Weekday; $k++){
-                            $calendar[$s]['day'] = '';
-                            $s++;
-                        }
-                    }
-                  
-                    $calendar[$s]['day'] = $i;
-                    $s++;
-
-                    if($i == $endDay){
-                        for($e = 0; $e < 6 - $endWeekday; $e++){
-                            $calendar[$s]['day'] = '';
-                            $s++;
-                        }  
-                    }
-                }
-                echo '<tr>';
-                $cnt = 0;
-                foreach($calendar as $key => $value){
-                    $cnt++;
-                    echo '<td>'.$value['day'].'</td>';
-          
-                    if($cnt == 7){
-                        echo '</tr><tr>';
-                        $cnt = 0;
-                    }
-                }
-                echo '</tr>';
-            ?>   
-        
-          </table>
+            <table border="1">
+                <tr>
+                    <th>SUN</th>
+                    <th>MON</th>
+                    <th>TUE</th>
+                    <th>WED</th>
+                    <th>THU</th>
+                    <th>FRI</th>
+                    <th>SAT</th>
+                </tr>
+                    <!--get array of month-->
+                    <?php $calendar = getCalendar($month)?>
+                <tr>
+                    <!-- make table of calendar -->
+                    <?php $cnt = 0; ?>
+                    <?php foreach ($calendar as $key => $value): ?>
+                        <?php $cnt++; ?>
+                        <td><?= $value; ?></td>
+                        <!-- make new line ever 7days-->
+                        <?php if ($cnt == 7): ?>
+                            </tr><tr>
+                            <?php $cnt = 0; ?>
+                        <?php endif; ?>
+                    <?php endforeach; ?>
+                </tr>
+            </table>
         <?php endfor; ?>
     </body>
 </html>
--- 01_intro/form_ivent.php
+++ 01_intro/form_ivent.php
@@ -9,15 +9,15 @@
     <table>
         <tr>
             <td>イベント日時</td>
-            <td><?php echo $_POST["day"]; ?></td>
+            <td><?php echo $_POST['day']; ?></td>
         </tr>
         <tr>
             <td>イベント名</td>
-            <td><?php echo $_POST["title"]; ?></td>
+            <td><?php echo $_POST['title']; ?></td>
         </tr>
         <tr>
             <td>イベント詳細</td>
-            <td><?php echo $_POST["details"]; ?></td>
+            <td><?php echo $_POST['details']; ?></td>
         </tr>
     </table>
 </body>
